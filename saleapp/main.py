03694b200f90461e74b75d009d0b95d124d7a032
AnhKhoa063 <52473815+AnhKhoa063@users.noreply.github.com>
2020-10-28 14:57:43 +0700

    init

---
+++ saleapp/main.py
@@ -0,0 +1,25 @@
+from flask import render_template
+
+from saleapp import app, utils
+
+
+@app.route("/")
+def index():
+    categories = utils.read_data()
+    return render_template('index.html', categories=categories)
+
+
+@app.route("/products")
+def product_list():
+    products = utils.read_data(path='data/product.json')
+    return render_template('products.html', products=products)
+
+
+@app.route("/products/<int:product_id>")
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id=product_id)
+    return render_template('product_detail.html', product=product)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
